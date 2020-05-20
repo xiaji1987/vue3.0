b0521dd0659b51c085d09fdfdf09c67bf0e2eb3c
xiaji1987 <3094889344@qq.com>
2020-05-20 16:34:10 +0800

    vue3.0管理项目

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="login">
+    login页面
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+#login {
+  height: 100vh;
+  background-color: #344a5f;
+}
+</style>
