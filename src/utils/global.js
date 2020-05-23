33eaed2195b520de3b10dcc3dd448872ce0cd6dd
xiaji1987 <3094889344@qq.com>
2020-05-23 15:56:52 +0800

    1.0.1版本开发

---
+++ src/utils/global.js
@@ -0,0 +1,34 @@
+// vue2.0
+// export default {
+//   install (Vue, options) {
+//     Vue.prototype.func = function () {
+//       console.log('全局函数')
+//     }
+//   }
+// }
+
+// vue3.0
+import { MessageBox } from 'element-ui';
+import { ref } from '@vue/composition-api';
+
+export function global() {
+  const str = ref('');
+  const confirm = (params) => {
+    MessageBox.confirm(params.content, params.tip || "提示", {
+      confirmButtonText: "确定",
+      cancelButtonText: "取消",
+      type: params.type || "warning",
+      center: true
+    }).then(() => {
+      str.value = params.id || '';
+      params.fn && params.fn(params.id || '')
+    }).catch(() => {
+      params.catchFn && params.catchFn()
+    });
+  }
+
+  return {
+    str,
+    confirm
+  }
+}
